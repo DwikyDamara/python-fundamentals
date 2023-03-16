b3a4e644d18a3e201db46d25fe7490b82bfeba60
Dwiky Damara <damaradwiky6@gmail.com>
2023-03-16 08:49:27 +0700

    Build the programmer joke code

--- main.py
+++ main.py
@@ -10,19 +10,21 @@ print('Mom said, "go to the store"')
 print('Budi said, "Ok!"')
 print('Mom said, "Buy me 1 bottle of milk, if there is an egg buy 6"')
 print('Mom said, "Ok!"')
-
+print("Budi walk to the store.")
 print("in the store!")
 milk_cost=1000
 egg_available=True
-if milk_cost>1000:
-    print("exit store")
-elif milk_cost==1000:
-    print("milk",1)
-if egg_available==False:
-    print("exit store")
-elif egg_available==True:
-    print("egg", 6)
-    print("exit store!")
+print('Budi ask to storekeeper"How much for 1 bottle of milk?"')
+print(f'Storekeeper said,"{milk_cost}"')
+if milk_cost==1000:
+    print('Budi said,"Nice, enough!"')
+    print('Budi said,"Then is there an egg"')
+    print('Storekeeper said,"Yes')
+    if egg_available==True:
+        print('Budi said,"buy 1 bottle of milk and 6 eggs"')
+    else:
+        print('Budi said,"just buy 1 bottle of milk"')
+print("exit store!")
 print("bring 1 bottle of milk and 6 egg to Mom")
 print('Mom said, "Nice"')
 
